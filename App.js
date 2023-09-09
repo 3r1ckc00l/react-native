863ebb5288699cec669c3cd55fc87595321687f9
3ryh_c00l <3rikpas@gmail.com>
2023-09-09 11:18:49 +0700

    initial commit

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  console.log("Apps executed!");
+
+  let x;
+  x.toSring();
+  return (
+    <View style={styles.container}>
+      <Text>This is React Native!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
